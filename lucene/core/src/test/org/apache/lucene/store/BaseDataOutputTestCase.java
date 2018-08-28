8b1cbaf0964f2874c75a3b4bddb259869972847a
Dawid Weiss <dweiss@apache.org>
2018-08-28 11:34:16 +0200

    Remove unused import.

--- lucene/core/src/test/org/apache/lucene/store/BaseDataOutputTestCase.java
+++ lucene/core/src/test/org/apache/lucene/store/BaseDataOutputTestCase.java
@@ -22,7 +22,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
